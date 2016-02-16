b0a43f84ccda80e0f4e845b2589710fa06669d6c
Lance Booth <lancecomputerwizz@gmail.com>
2016-02-16 14:16:31 -0800

    Added function for drawing a single tetrimino.

--- src/tetris/TetrisManager.cpp
+++ src/tetris/TetrisManager.cpp
@@ -60,8 +60,46 @@ void tetrisGameManager::drawWell(sf::RenderWindow& window)
     }*/
 }
 
+void tetrisGameManager::drawTetrimino(sf::RenderWindow& window)
+{
+    switch(blockGrid[x][y])
+    {
+    case Empty:
+        block.setFillColor(sf::Color::Black);
+        block.setOutlineColor(sf::Color::Black);
+        break;
+    case Cyan:
+        block.setFillColor(sf::Color::Cyan);
+        block.setOutlineColor(sf::Color(96, 216, 220, 255));
+        break;
+    case Yellow:
+        block.setFillColor(sf::Color::Yellow);
+        block.setOutlineColor(sf::Color(222, 222, 79, 255));
+        break;
+    case Orange:
+        block.setFillColor(sf::Color(255, 166, 31, 255));
+        block.setOutlineColor(sf::Color(192, 124, 22, 255));
+        break;
+    case Blue:
+        block.setFillColor(sf::Color::Blue);
+        block.setOutlineColor(sf::Color(23, 19, 138, 255));
+        break;
+    case Green:
+        block.setFillColor(sf::Color::Green);
+        block.setOutlineColor(sf::Color(42, 183, 37, 255));
+        break;
+    case Red:
+        block.setFillColor(sf::Color::Red);
+        block.setOutlineColor(sf::Color(195, 25, 30, 255));
+        break;
+    case Purple:
+        block.setFillColor(sf::Color(239, 68, 245, 255));
+        block.setOutlineColor(sf::Color(181, 69, 195, 255));
+        break;
+    }
+}
+
 void tetrisGameManager::updateGrid(sf::RenderWindow& window)
 {
-
 
 }
